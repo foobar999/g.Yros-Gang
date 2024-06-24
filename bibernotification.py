bb849b52322a466e4f4df6cdfac895fdf997f8a4
foobar999 <foobar999@users.noreply.github.com>
2024-06-24 11:52:54 +0200

    Update bibernotification.py

--- bibernotification.py
+++ bibernotification.py
@@ -2,7 +2,7 @@ import instaloader
 
 def main():
   L = instaloader.Instaloader()
-  profile = instaloader.Profile.from_username(L.context, "realpostillon")
+  profile = instaloader.Profile.from_username(L.context, "@realpostillon")
   print("Profile:{profile}".format(profile = profile))
 
 if __name__ == "__main__":
